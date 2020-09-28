312af500eb7ce33d142c195f736e270f7777be12
Maria Riazanova <mv_13@mail.ru>
2020-09-29 00:33:45 +0300

    feat: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  let dreamTeam = [];
+  
+  if (Array.isArray(members)){
+    for (let i = 0; i < members.length; i++){
+      if (typeof members[i] === "string"){
+        dreamTeam.push(members[i].trim().toUpperCase()[0]);
+      }
+    }
+    if (dreamTeam != []){
+      return dreamTeam.sort().join("");
+    } else {
+      return false
+    }
+  } else {
+    return false;
+  }
+  
+  
+  
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
