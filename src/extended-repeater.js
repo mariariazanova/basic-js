d28a246e0e053848ed6e973e1e448f8dd620904c
Maria Riazanova <mv_13@mail.ru>
2020-10-01 21:09:33 +0300

    2 more

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater( str, options ) {
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let result = '';
+  let additionSeparator = '|';
+  let separator = '+';
+  
+  if (typeof str != "string") {
+    str = str + "";
+  };
+
+  //if (typeof options.addition != "string") {
+  //  options.addition = options.addition + "";
+  //};
+
+  if(options.addition === undefined){
+      options.addition = '';
+  };
+  if(options.additionRepeatTimes === undefined) {
+      options.additionRepeatTimes = 1;
+  };
+  if(options.repeatTimes === undefined) {
+      options.repeatTimes = 1;
+  };
+  if (options.additionSeparator) {
+      additionSeparator = options.additionSeparator;
+  };
+  if (options.separator) {
+      separator = options.separator;
+  };
+
+
+  for (let i = 1; i<=options.repeatTimes; i++) {
+    result += str;
+    for (let j = 1; j<=options.additionRepeatTimes; j++) {
+      result += options.addition;
+      if(j < options.additionRepeatTimes) result += additionSeparator;
+    };
+    if(i < options.repeatTimes) result += separator;
+  }
+
+  return result;
 };
   
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,9 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
+  
+    let calculationObject = {};
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    calculationObject.turns = Math.pow(2, disksNumber) - 1;
+    calculationObject.seconds = Math.floor(calculationObject.turns / (turnsSpeed / 3600));
+    
+    return calculationObject;
 };
