596116da25c71cc23a765c5d1db82b6a621a0188
Maria Riazanova <mv_13@mail.ru>
2020-10-04 22:19:52 +0300

    feat: weather done

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined) { //дата не введена
+    return 'Unable to determine the time of year!'
+  }
+  if (isNaN(date)) { //дата не валидна
+    throw new Error()
+  } 
+  var monthNumber = date.getMonth(); //возвращает номер месяца
+  if (monthNumber < 2 || monthNumber == 11) {
+    return "winter"
+  } else 
+      if (monthNumber >=2 && monthNumber < 5) {
+        return "spring"
+      } else 
+          if (monthNumber >=5 && monthNumber < 8) {
+            return "summer"
+          } else 
+              if (monthNumber >=8 && monthNumber < 11) {
+                return "fall"
+              }
 };
