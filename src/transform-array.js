17738208d961010ab98185b461cfa82e83ed54fb
Maria Riazanova <mv_13@mail.ru>
2020-10-04 18:31:15 +0300

    feat: transform done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+	if(!Array.isArray(arr)) {
+		throw Error();
+	} 
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  var answer = [];
+  for(var i = 0; i < arr.length; ++i) {
+		switch (arr[i]) {	 
+		  case "--discard-next":
+		  		i++;
+		    	break;
+		  case "--discard-prev":
+		    if(i > 0 && arr[i - 2] !== '--discard-next') {
+		    	answer.pop();
+		    }
+		    break;
+		  case "--double-next":
+		    if(i < arr.length - 1) {
+		    	answer.push(arr[i + 1]);
+		    }
+		    break;
+		  case "--double-prev":
+		    if(i > 0 && arr[i - 2] !== '--discard-next') {
+		    	answer.push(arr[i - 1]);
+		    }
+		    break;
+		    default:
+	  			answer.push(arr[i]);
+		}
+  }
+  return answer;
 };
